e67638e5f4631a40cb7c71d0611c5ec467ac41a0
KaloyanDanev <kaloian4obg99@gmail.com>
2019-11-15 22:40:07 +0200

    Filtering by price, size, breakfast and pets

--- src/context.js
+++ src/context.js
@@ -55,7 +55,7 @@ export default class RoomProvider extends Component {
 
     handleChange = event => {
         const target = event.target;
-        const value = event.type === 'checkbox' ? target.checked:target.value;
+        const value = target.type === 'checkbox' ? target.checked:target.value;
         const name = event.target.name;
         this.setState({
             [name]:value
@@ -68,6 +68,7 @@ export default class RoomProvider extends Component {
            let tempRooms = [...rooms];
 
            capacity = parseInt(capacity);
+           price =  parseInt(price);
 
            if (type !== "all") {
                tempRooms = tempRooms.filter(room => room.type === type);
@@ -75,6 +76,18 @@ export default class RoomProvider extends Component {
 
            if (capacity !== 1){
                tempRooms = tempRooms.filter(room => room.capacity >= capacity)
+           }
+
+           tempRooms = tempRooms.filter(room => room.price <= price);
+
+           tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+
+           if (breakfast){
+               tempRooms = tempRooms.filter(room => room.breakfast === true)
+           }
+
+           if (pets){
+               tempRooms = tempRooms.filter(room => room.pets === true)
            }
 
         this.setState({
