bafc0e765fba715228f150b93ff94b561f87b591
KaloyanDanev <kaloian4obg99@gmail.com>
2019-11-15 20:13:43 +0200

    All the rooms are are showed on the rooms page. start of the filtering on the rooms

--- src/context.js
+++ src/context.js
@@ -7,17 +7,32 @@ export default class RoomProvider extends Component {
         rooms:[],
         sortedRooms:[],
         featuredRooms:[],
-        loading:true
+        loading:true,
+        type:'all',
+        capacity:1,
+        price:0,
+        minPrice:0,
+        maxPrice:0,
+        minSize:0,
+        maxSize:0,
+        breakfast:false,
+        pets:false
     };
 
     componentDidMount() {
         let rooms = this.formatData(items);
         let featuredRooms = rooms.filter(room => room.featured === true);
+        let maxPrice = Math.max(...rooms.map(item => item.price));
+        let maxSize = Math.max(...rooms.map(item => item.size));
+
         this.setState({
             rooms,
             featuredRooms,
             sortedRooms:rooms,
-            loading:false
+            loading:false,
+            price:maxPrice,
+            maxPrice,
+            maxSize
         });
     }
 
@@ -38,8 +53,21 @@ export default class RoomProvider extends Component {
         return room;
     };
 
+    handleChange = event => {
+        const target = event.target;
+        const value = event.type === 'checkbox' ? target.checked:target.value;
+        const name = event.target.name;
+        this.setState({
+            [name]:value
+        },this.filterRooms)
+    };
+
+    filterRooms = ()=> {
+
+    };
+
     render() {
-        return <RoomContext.Provider value={{...this.state,getRoom:this.getRoom}}>
+        return <RoomContext.Provider value={{...this.state,getRoom:this.getRoom,handleChange:this.handleChange}}>
             {this.props.children}
         </RoomContext.Provider>;
     }
